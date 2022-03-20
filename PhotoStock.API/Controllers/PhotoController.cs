7497b17bbd5c100f103c2e39820a4dc361dfcb59
Cemal Salih Bulak <cemal.bulak@digilera.com>
2022-03-20 18:09:59 +0300

    ..

--- PhotoStock.API/Controllers/PhotoController.cs
+++ PhotoStock.API/Controllers/PhotoController.cs
@@ -12,21 +12,31 @@ namespace PhotoStock.API.Controllers
     public class PhotoController : CustomBaseController
     {
         [HttpPost]
-        public async Task<IActionResult> PhotoSave(IFormFile photo, CancellationToken cancellationToken)
+        public async Task<IActionResult> PhotoSave(List<IFormFile> photo, CancellationToken cancellationToken)
         {
-            if (photo is not { Length: > 0 })
-                return CreateActionResultInstance(Response<PhotoDto>.Fail("Photo is empty", 400));
-            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photo.FileName);
+            if (photo.Count <= 0) return CreateActionResultInstance(Response<PhotoDto>.Fail("Photo is empty", 400));
+            
+            var photoDto = new List<PhotoDto>();
 
-            await using var stream = new FileStream(path, FileMode.Create);
-            await photo.CopyToAsync(stream, cancellationToken);
+            foreach (var formFile in photo)
+            {
+                if (formFile is not { Length: > 0 })
+                {
+                    return CreateActionResultInstance(Response<PhotoDto>.Fail("Photo is empty", 400));
+                }
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", formFile.FileName);
+                await using var stream = new FileStream(path, FileMode.Create);
+                await formFile.CopyToAsync(stream, cancellationToken);
 
-            //http://www.photostock.api.com/photos/aasdasd.jpg
-            var returnPath = "photos/" + photo.FileName;
+                //http://www.photostock.api.com/photos/aasdasd.jpg
+                var returnPath = "photos/" + formFile.FileName;
 
-            PhotoDto photoDto = new() { Url = returnPath };
-
-            return CreateActionResultInstance(Response<PhotoDto>.Success(photoDto, 204));
+                photoDto.Add(new PhotoDto()
+                {
+                    Url = returnPath
+                });
+            }
+            return CreateActionResultInstance(Response<List<PhotoDto>>.Success(photoDto, 200));
 
         }
 
@@ -37,7 +47,6 @@ namespace PhotoStock.API.Controllers
 
             if (!System.IO.File.Exists(path))
                 return CreateActionResultInstance(Response<NoContent>.Fail("Photo not found", 404));
-
 
             System.IO.File.Delete(path);
             return CreateActionResultInstance(Response<NoContent>.Success(204));
