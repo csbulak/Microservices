e7fd484013f40e3b2fbced2170079999e770e080
Cemal Salih Bulak <cemal.bulak@digilera.com>
2022-04-25 03:18:50 +0300

    69. DiscountService

---
+++ Services/Discount/Discount.API/Services/DiscountService.cs
@@ -0,0 +1,85 @@
+﻿using System.Data;
+using Dapper;
+using Npgsql;
+using Shared.Dtos;
+
+namespace Discount.API.Services;
+
+public class DiscountService : IDiscountService
+{
+    private readonly IConfiguration _configuration;
+    private readonly IDbConnection _connection;
+
+    public DiscountService(IConfiguration configuration, IDbConnection connection)
+    {
+        _configuration = configuration;
+        _connection = new NpgsqlConnection(_configuration.GetConnectionString("PostgreSql"));
+    }
+
+    public async Task<Response<List<Models.Discount>>> GetAll()
+    {
+        var discount = await _connection.QueryAsync<Models.Discount>("select * from discount");
+        return Response<List<Models.Discount>>.Success(discount.ToList(), 200);
+    }
+
+    public async Task<Response<Models.Discount>> GetById(int id)
+    {
+        var discount = (await _connection.QueryAsync<Models.Discount>("select * from discount where id = @id", new
+        {
+            id = id
+        })).SingleOrDefault();
+
+        return discount == null
+            ? Response<Models.Discount>.Fail("not found",
+                404)
+            : Response<Models.Discount>.Success(discount,
+                200);
+    }
+
+    public async Task<Response<NoContent>> Save(Models.Discount discount)
+    {
+        var status = await _connection.ExecuteAsync(
+            "insert into discount (userid,rate,code) values(@UserId,@Rate,@Code)", discount);
+
+        return status > 0 ? Response<NoContent>.Success(204) : Response<NoContent>.Fail("an error accrued while adding.", 500);
+    }
+
+    public async Task<Response<NoContent>> Update(Models.Discount discount)
+    {
+        var status = await _connection.ExecuteAsync(
+            "update discount set userid=@UserId,rate=@Rate,code=@Code where id = @Id", new
+            {
+                UserId = discount.UserId,
+                Rate = discount.Rate,
+                Code = discount.Code,
+                Id = discount.Id
+            });
+
+        return status > 0 ? Response<NoContent>.Success(200) : Response<NoContent>.Fail("discount not found", 404);
+
+    }
+
+    public async Task<Response<NoContent>> Delete(int id)
+    {
+        var status = await _connection.ExecuteAsync("delete from discount where id=@Id", new { Id = id });
+
+        return status > 0 ? Response<NoContent>.Success(200) : Response<NoContent>.Fail("discount not found", 404);
+    }
+
+    public async Task<Response<Models.Discount>> GetByCodeAndUserId(string code, string userId)
+    {
+        var discounts = await _connection.QueryAsync("select * from discount where code = @Code and userid=@UserId", new
+        {
+            Code = code,
+            UserId = userId
+        });
+
+        var hasDiscount = discounts.FirstOrDefault();
+
+        return hasDiscount == null
+            ? Response<Models.Discount>.Fail("not found",
+                404)
+            : (Response<Models.Discount>)Response<Models.Discount>.Success(hasDiscount,
+                200);
+    }
+}
