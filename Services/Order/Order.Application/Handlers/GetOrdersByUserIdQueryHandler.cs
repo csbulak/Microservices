fe79b71e96bbcee28bf90a5fae7c30c4211b10ee
Cemal Salih Bulak <cemal.bulak@digilera.com>
2022-04-27 01:44:11 +0300

    89. MediatR/Query/QueryHandler

---
+++ Services/Order/Order.Application/Handlers/GetOrdersByUserIdQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Order.Application.Dtos;
+using Order.Application.Mapping;
+using Order.Application.Queries;
+using Order.Infrastructure;
+using Shared.Dtos;
+
+namespace Order.Application.Handlers;
+
+public class GetOrdersByUserIdQueryHandler : IRequestHandler<GetOrdersByUserIdQuery, Response<List<OrderDto>>>
+{
+    private readonly OrderDbContext _dbContext;
+
+    public GetOrdersByUserIdQueryHandler(OrderDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<Response<List<OrderDto>>> Handle(GetOrdersByUserIdQuery request, CancellationToken cancellationToken)
+    {
+        var orders = await _dbContext.Orders.Include(x => x.OrderItems)
+            .Where(x => x.BuyerId == request.UserId)
+            .ToListAsync(cancellationToken: cancellationToken);
+
+        if (!orders.Any())
+        {
+            return Response<List<OrderDto>>.Success(new List<OrderDto>(), 200);
+        }
+
+        var ordersDto = ObjectMapper.Mapper.Map<List<OrderDto>>(orders);
+
+        return Response<List<OrderDto>>.Success(ordersDto, 200);
+    }
+}
