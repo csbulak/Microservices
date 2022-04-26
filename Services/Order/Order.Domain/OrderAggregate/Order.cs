6784c4e79bc8a76100c42f5d6251de124842f668
Cemal Salih Bulak <cemal.bulak@digilera.com>
2022-04-26 04:15:15 +0300

    ..

---
+++ Services/Order/Order.Domain/OrderAggregate/Order.cs
@@ -0,0 +1,35 @@
+﻿using Order.Domain.Core;
+
+namespace Order.Domain.OrderAggregate;
+
+public class Order : Entity, IAggregateRoot
+{
+    public Order(Address address, string buyerId)
+    {
+        _orderItems = new List<OrderItem>();
+        CreatedTime = DateTime.Now;
+        Address = address;
+        BuyerId = buyerId;
+    }
+
+    public DateTime CreatedTime { get; private set; }
+    public Address Address { get; private set; }
+    public string BuyerId { get; private set; }
+
+    private readonly List<OrderItem> _orderItems; //Backing Field
+
+    public IReadOnlyCollection<OrderItem> OrderItems => _orderItems;
+
+    public void AddOrderItem(string productId, string productName, decimal price, string pictureUrl)
+    {
+        var existProduct = _orderItems.Any(x => x.ProductId == productId);
+
+        if (!existProduct)
+        {
+            var newOrderItem = new OrderItem(productId, productName, pictureUrl, price);
+            _orderItems.Add(newOrderItem);
+        }
+    }
+
+    public decimal GetTotalPrice => _orderItems.Sum(x => x.Price);
+}
