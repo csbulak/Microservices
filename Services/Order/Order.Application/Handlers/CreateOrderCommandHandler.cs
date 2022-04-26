0dbd22556a037a5a11376dfab2e551a2f5f78b86
Cemal Salih Bulak <cemal.bulak@digilera.com>
2022-04-27 01:54:50 +0300

    90. MediatR/Command/CommandHandler

---
+++ Services/Order/Order.Application/Handlers/CreateOrderCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using Order.Application.Commands;
+using Order.Application.Dtos;
+using Order.Domain.OrderAggregate;
+using Order.Infrastructure;
+using Shared.Dtos;
+
+namespace Order.Application.Handlers;
+
+public class CreateOrderCommandHandler : IRequestHandler<CreateOrderCommand, Response<CreatedOrderDto>>
+{
+    private readonly OrderDbContext _orderDbContext;
+
+    public CreateOrderCommandHandler(OrderDbContext orderDbContext)
+    {
+        _orderDbContext = orderDbContext;
+    }
+
+    public async Task<Response<CreatedOrderDto>> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+    {
+        var newAddress = new Address(request.Address.Province, request.Address.District, request.Address.Street,
+            request.Address.ZipCode, request.Address.Line);
+
+        var newOrder = new Domain.OrderAggregate.Order(newAddress, request.BuyerId);
+
+        request.OrderItems.ForEach(x =>
+        {
+            newOrder.AddOrderItem(x.ProductId, x.ProductName, x.Price, x.PictureUrl);
+        });
+
+        await _orderDbContext.Orders.AddAsync(newOrder, cancellationToken);
+        await _orderDbContext.SaveChangesAsync(cancellationToken);
+
+        return Response<CreatedOrderDto>.Success(new CreatedOrderDto() { OrderId = newOrder.Id }, 200);
+
+    }
+}
