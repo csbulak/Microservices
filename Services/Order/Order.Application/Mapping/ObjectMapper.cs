8778d82995bcf11ace59d90e2abefc6d8aee4fdd
Cemal Salih Bulak <cemal.bulak@digilera.com>
2022-04-27 01:22:54 +0300

    88. AutoMapper

---
+++ Services/Order/Order.Application/Mapping/ObjectMapper.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+
+namespace Order.Application.Mapping;
+
+public static class ObjectMapper
+{
+    private static readonly Lazy<IMapper> Lazy = new(() =>
+    {
+        var config = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<CustomMapping>();
+        });
+
+        return config.CreateMapper();
+    }); // İstendiği zaman initialize eder.
+
+    public static IMapper Mapper => Lazy.Value; // Mapper çağrılana kadar yukarıdaki kodlar çalışmaz.
+}
