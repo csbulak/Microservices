a0450e5e66efb7277f534052c3b84c05d35af009
Cemal Salih Bulak <cemal.bulak@digilera.com>
2022-04-28 02:17:48 +0300

    112. IIdentityService/IdentityModel Library

---
+++ Client/WebUI/Models/SignInInput.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebUI.Models;
+
+public class SignInInput
+{
+    [Display(Name = "Email Adresi")]
+    public string Email { get; set; }
+    [Display(Name = "Şifre")]
+    public string Password { get; set; }
+    [Display(Name = "Beni Hatırla")]
+    public bool IsRemember { get; set; }
+}
---
+++ Client/WebUI/Services/Interfaces/IIdentityService.cs
@@ -0,0 +1,12 @@
+﻿using IdentityModel.Client;
+using Shared.Dtos;
+using WebUI.Models;
+
+namespace WebUI.Services.Interfaces;
+
+public interface IIdentityService
+{
+    Task<Response<bool>> SignIn(SignInInput signInInput);
+    Task<TokenResponse> GetAccessTokenByRefreshToken();
+    Task RemoveRefreshToken();
+}
