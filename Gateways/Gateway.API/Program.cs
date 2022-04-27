45d6c662995a91588449727d351f8c8ed7d633b0
Cemal Salih Bulak <cemal.bulak@digilera.com>
2022-04-27 03:23:29 +0300

    106. configuration.development.json

--- Gateways/Gateway.API/Program.cs
+++ Gateways/Gateway.API/Program.cs
@@ -1,11 +1,14 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
+IConfiguration configuration = new ConfigurationBuilder()
+    .AddJsonFile("configuration.development.json")
+    .Build();
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOcelot(configuration);
+
 var app = builder.Build();
-
-builder.Services.AddOcelot();
-
-await app.UseOcelot();
-
+app.UseOcelot();
 app.Run();
