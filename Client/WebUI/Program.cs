e77902b7264d3e53a8fb9400cb7d8dc4a4471c3c
Cemal Salih Bulak <cemal.bulak@digilera.com>
2022-04-28 03:20:17 +0300

    118. Startup/Cookie settings

--- Client/WebUI/Program.cs
+++ Client/WebUI/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using WebUI.Models;
+using WebUI.Services;
 using WebUI.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,8 +10,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddHttpClient<IIdentityService>();
 
+builder.Services.AddScoped<IIdentityService, IdentityService>();
+
 builder.Services.Configure<ServiceApiSettings>(builder.Configuration.GetSection("ServiceApiSettings"));
 builder.Services.Configure<ClientSettings>(builder.Configuration.GetSection("ClientSettings"));
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(CookieAuthenticationDefaults.AuthenticationScheme,
+    opt =>
+    {
+        opt.LoginPath = "/Auth/SignIn";
+        opt.ExpireTimeSpan = TimeSpan.FromDays(60);
+        opt.SlidingExpiration = true;
+        opt.Cookie.Name = "WebUICookie";
+    });
 
 builder.Services.AddControllersWithViews();
 
@@ -24,6 +37,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
