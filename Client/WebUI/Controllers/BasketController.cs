7821f028321405a2f01ab03f3a768b14049a6b8e
Cemal Salih Bulak <cemal.bulak@digilera.com>
2022-05-02 09:08:57 +0300

    155. BasketController

---
+++ Client/WebUI/Controllers/BasketController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebUI.Models.Basket;
+using WebUI.Services.Interfaces;
+
+namespace WebUI.Controllers;
+
+public class BasketController : Controller
+{
+    private readonly IBasketService _basketService;
+    private readonly ICatalogService _catalogService;
+
+    // GET
+    public BasketController(IBasketService basketService, ICatalogService catalogService)
+    {
+        _basketService = basketService;
+        _catalogService = catalogService;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var basket = await _basketService.Get();
+        return View(basket);
+    }
+
+    public async Task<IActionResult> AddBasketItem(string courseId)
+    {
+        var course = await _catalogService.GetCourseById(courseId);
+
+        var basketItemViewModel = new BasketItemViewModel()
+        {
+            CourseId = course.Id,
+            CourseName = course.Name,
+            Price = course.Price,
+            Quantity = 1
+        };
+
+        await _basketService.AddBasketItem(basketItemViewModel);
+
+        return RedirectToAction("Index");
+    }
+
+    public async Task<IActionResult> RemoveBasketItem(string courseId)
+    {
+        var response = await _basketService.RemoveBasketItem(courseId);
+        return RedirectToAction("Index");
+    }
+}
