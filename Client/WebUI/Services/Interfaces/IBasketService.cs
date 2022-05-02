a05320db2041afdb1eccd9a02185534128162c10
Cemal Salih Bulak <cemal.bulak@digilera.com>
2022-05-02 08:30:11 +0300

    152. IBasketService

---
+++ Client/WebUI/Services/Interfaces/IBasketService.cs
@@ -0,0 +1,16 @@
+﻿using WebUI.Models.Basket;
+
+namespace WebUI.Services.Interfaces;
+
+public interface IBasketService
+{
+    Task<bool> SaveOrUpdate(BasketViewModel basketViewModel);
+    Task<bool> Delete();
+
+    Task<BasketViewModel> Get();
+
+    Task AddBasketItem(BasketItemViewModel basketItemViewModel);
+    Task<bool> RemoveBasketItem(string courseId);
+    Task<bool> ApplyDiscount(string discountCode);
+    Task<bool> CancelApplyDiscount();
+}
