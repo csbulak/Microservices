80b885f596ea585460326233cc9ae16ad8c96b09
Cemal Salih Bulak <cemal.bulak@digilera.com>
2022-05-02 08:25:41 +0300

    151. Basket microservice için viewmodel oluşturma

---
+++ Client/WebUI/Models/Basket/BasketViewModel.cs
@@ -0,0 +1,32 @@
+﻿namespace WebUI.Models.Basket;
+
+public class BasketViewModel
+{
+    public string UserId { get; set; }
+    public string DiscountCode { get; set; }
+    public int? DiscountRate { get; set; }
+
+    public List<BasketItemViewModel> _basketItems { get; set; }
+
+    private List<BasketItemViewModel> BasketItems
+    {
+        get
+        {
+            if (HasDiscount)
+            {
+                _basketItems.ForEach(x =>
+                {
+                    var discountPrice = x.Price * ((decimal)DiscountRate / 100);
+                    x.AppliedDiscount(Math.Round(x.Price - discountPrice, 2));
+                });
+            }
+
+            return _basketItems;
+        }
+        set => _basketItems = value;
+    }
+
+    public decimal TotalPrice => BasketItems.Sum(x => x.GetCurrentPrice * x.Quantity);
+
+    public bool HasDiscount => !string.IsNullOrEmpty(DiscountCode);
+}
